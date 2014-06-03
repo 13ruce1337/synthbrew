8e79c5cb3c2c7363b93cc7e67044465b615fc940
13ruce1337 <tagcoop@gmail.com>
2014-06-03 04:43:17 -0400

    added dev env

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,38 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    execute: {
+      build: {
+        src: [
+          'src/bin/compile.js'
+        ]
+      },
+      testbuild: {
+        src: [
+          'test/compile.js'
+        ]
+      }
+    },
+    watch: {
+      scripts: {
+        files: [
+          'src/*.*',
+          'test/*.*'
+        ]
+      }
+    },
+    open: {
+      dev: {
+        path: __dirname + '/test/test.html',
+        app: 'Google Chrome'
+      }
+    }
   });
 
+  grunt.loadNpmTasks('grunt-execute');
+  grunt.loadNpmTasks('grunt-open');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('default',[]);
-  grunt.registerTask('test',[]);
+  grunt.registerTask('default',['execute:build']);
+  grunt.registerTask('test',['execute:testbuild', 'open', 'watch']);
 };
