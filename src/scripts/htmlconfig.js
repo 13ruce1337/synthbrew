dec21f3a6c89464d16749e50cd40fb401b4da4f3
13ruce1337 <tagcoop@gmail.com>
2014-06-03 04:13:03 -0400

    hogan compiles apps

--- src/scripts/htmlconfig.js
+++ src/scripts/htmlconfig.js
@@ -1,29 +1,32 @@
-var fs = require('fs'),
-    url,
-    enconding,
-    synthbrew,
-    partials;
-
-encoding = 'utf8';
-partials = {
-  app: undefined
-};
-
-function readfile(file, title, cb) {
-  url = __dirname + file;
-  fs.readFile(file, encoding, function(err, data) {
-    if(err) throw err;
-    partials[title] = data;
-    cb();
-  });
-}
-
-function done() {
-  var done = true;
-  for(var key in partials) {
-    if(!partials[key]) done = false;
+module.exports = (function() {
+  var fs = require('fs'),
+      url,
+      enconding,
+      synthbrew,
+      partials;
+  
+  encoding = 'utf8';
+  partials = {
+    app: undefined
+  };
+  
+  function readfile(file, title, cb) {
+    url = __dirname + file;
+    fs.readFile(file, encoding, function(err, data) {
+      if(err) throw err;
+      partials[title] = data;
+      cb();
+    });
   }
-  if(done) module.exports = partials;
-}
-
-readfile(__dirname + '/../libs/app.html', 'app', done);
+  
+  function done() {
+    var done = true;
+    for(var key in partials) {
+      if(!partials[key]) done = false;
+    }
+    if(done) return true;
+  }
+  
+  readfile(__dirname + '/../libs/app.html', 'app', done);
+  return partials;
+})();
